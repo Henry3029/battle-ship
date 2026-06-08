3037fd93ebea2cdbb7cd2f98c39a0d50924b1951
Henry chigozie Goodluck <chigoziehenry852@gmail.com>
2026-06-08 22:03:37 +0100

    modify gameboard file

--- src/models/gameBoard.test.js
+++ src/models/gameBoard.test.js
@@ -1,9 +1,8 @@
-/*import { GameBoard } from './gameBoard'; 
+import { GameBoard } from './gameBoard'; 
 import { Ship } from './ship'; 
 
 test("GameBoard should place ships at specific coordinates", () => {
 	const myBoard = new GameBoard()
 	myBoard.placeShip(0, 0, 3, "horizontal");
 	expect(myBoard.shipPlaced["0,0"]).toBeInstanceOf(ship);
-	
-	} */
+	} 
