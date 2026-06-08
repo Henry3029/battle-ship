4c4f76d177c50d5956383d45c47a283ca7803d87
Henry chigozie Goodluck <chigoziehenry852@gmail.com>
2026-06-08 12:34:27 +0100

    modify ship.test.file

--- src/models/ship.test.js
+++ src/models/ship.test.js
@@ -5,3 +5,10 @@ test("let the hit count increase, when we can the hit() method", () => {
 	myShip.hit();
 	expect(myShip.damage).toBe(1);
 	})
+	
+// 2	
+	test("when hit equal length, let isSunk be true", () => {
+		const myShip = new Ship(3)
+		myShip.isSunk();
+		expect(shipStat.isSunk).toBe(true);
+		})
