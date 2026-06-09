7bb3b85df1e31acd8f7aa8a10123da99e59fcaeb
Henry chigozie Goodluck <chigoziehenry852@gmail.com>
2026-06-09 10:50:45 +0100

    testing

--- src/models/gameBoard.test.js
+++ src/models/gameBoard.test.js
@@ -1,8 +1,7 @@
-import { GameBoard } from './gameBoard'; 
-import { Ship } from './ship'; 
-
 test("GameBoard should place ships at specific coordinates", () => {
-	const myBoard = new GameBoard()
-	myBoard.placeShip(0, 0, 3, "horizontal");
-	expect(myBoard.shipPlaced["0,0"]).toBeInstanceOf(ship);
-	} 
+  const myBoard = new GameBoard();
+  myBoard.placeShip(0, 0, 3, "horizontal");
+  
+  // Notice the closing parenthesis right after the ["0,0"] bracket!
+  expect(myBoard.shipPlaced["0,0"]).toBeInstanceOf(Ship); 
+});
