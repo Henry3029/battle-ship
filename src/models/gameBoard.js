f8fb2df9882d786a94bbd4d9b30d63ed8b3711e7
Henry chigozie Goodluck <chigoziehenry852@gmail.com>
2026-06-09 10:21:53 +0100

    testing

--- src/models/gameBoard.js
+++ src/models/gameBoard.js
@@ -1,18 +1,21 @@
 import { Ship } from './ship'; 
 
 export class GameBoard {
-	constructor() {
-		this.shipPlaced = {}
-		}
-		
-		placeShip(row, col, length, orientation) {
-			const myShip = new Ship(length);
-			for(let i = 0; i < length; i++) {
-				if(orientation === 'horizontal') {
-			this.shipPlaced[`${row},${col + i}] = myShip; 
-			} else if(orientation === 'vertical') {
-				this.shipPlaced[`${row + i},${col}] = myShip; 
-				}
-			}
-			}
-	}
+  constructor() {
+    this.shipPlaced = {};
+  }
+    
+  placeShip(row, col, length, orientation) {
+    const myShip = new Ship(length);
+    
+    for (let i = 0; i < length; i++) {
+      if (orientation === 'horizontal') {
+        // Fixed: Added the closing backtick right before the closing bracket
+        this.shipPlaced[`${row},${col + i}`] = myShip; 
+      } else if (orientation === 'vertical') {
+        // Fixed: Added the closing backtick right before the closing bracket
+        this.shipPlaced[`${row + i},${col}`] = myShip; 
+      }
+    }
+  }
+}
