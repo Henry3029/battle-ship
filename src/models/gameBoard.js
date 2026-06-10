6117eb0bc3b02d03f534d347fae0ca424e37fac3
Henry chigozie Goodluck <chigoziehenry852@gmail.com>
2026-06-10 22:51:23 +0100

    create an isAllShipSunk method

--- src/models/gameBoard.js
+++ src/models/gameBoard.js
@@ -4,10 +4,13 @@ export class GameBoard {
   constructor() {
     this.shipPlaced = {};
     this.missedShots = [];
+    this.ships = [];
   }
     
   placeShip(row, col, length, orientation) {
     const myShip = new Ship(length);
+    
+    this.ships.push(myShip);
     
     for (let i = 0; i < length; i++) {
       if (orientation === 'horizontal') {
@@ -30,5 +33,26 @@ export class GameBoard {
       this.missedShots.push(key);
     }
   } // Closes receiveAttack
+  
+  allShipsSunk() {
+  	if(this.ships.length === 0) return false; // no ship has been placed.
+  
+  for(let i = 0; i < this.ships.length; i++) {
+  	if(this.ships[i].damage < this.ships[i].length) {
+  console.log("One Ship is Still Floating");
+  return false; 
+  }
+  
+  // if the loop check the whole ship,  and they are all busted.
+  return true;
+  	}
+  
+  
+  	}
+  
+  
+  
+  
+  
 
 } // FIXED: This final brace closes the entire GameBoard class
